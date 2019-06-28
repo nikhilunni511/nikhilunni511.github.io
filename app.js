58b82bdd1d9161a8f9c7ddfa89a49fd54383cded
nikhilunni511 <nikhi.unni@gmail.com>
2019-06-28 14:25:44 +0530

    added

--- app.js
+++ app.js
@@ -10,20 +10,45 @@ var fs = require('fs');
  
 
  
-fs.readdir(__dirname+'/storage/downloads/movies', function(err, items) {
-    console.log(items);
+// fs.readdir(__dirname+'/storage/downloads/movies', function(err, items) {
+//     console.log(items);
  
-    for (var i=0; i<items.length; i++) {
-        console.log(items[i]);
-    }
-});
+//     for (var i=0; i<items.length; i++) {
+//         console.log(items[i]);
+//     }
+// });
 
-// app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
     res.setHeader("Content-type", "text/html");
-    res.sendFile(path.join(__dirname, './', 'server.html'));
+    var html = `<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        `;
+        fs.readdir(__dirname+'/storage/downloads/movies', function(err, items) {
+            console.log(items);
+         
+            for (var i=0; i<items.length; i++) {
+                console.log(typeof items[i]);
+                html+=`<a href="/download?id=${items[i]}">kbjbjbhj</a><br /><br />`
+            }
+            html += `<p>Welcome</p>
+    </body>
+    </html>`
+    res.end(html);
+        });
+    
+    // res.end(html);
 })
+app.get('/download', function(req, res){
+    const file = `${__dirname}/'/storage/downloads/movies'/${req.query.id}`;
+    res.download(file); // Set disposition and send it.
+  });
 // app.get('/:id/download', function (req, res, next) {
 //     var filePath = "/my/file/path/..."; // Or format the path using the `id` rest param
 //     var fileName = "report.pdf"; // The default name the browser will use
